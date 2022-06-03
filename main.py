3e42d6e0fb2fd762f7cdeebf3fec417b58ed4a04
matthew <matthew@selfdecode.com>
2022-06-02 21:09:16 -0700

    final version

--- main.py
+++ main.py
@@ -1,49 +1,7 @@
-"""
-need a 10x10 grid
-for each episode, random init grid with cans and robot
-
-each step during an episode:
-
-observe the state
-choose an action (greedy action selection)
-perform action
-recieve reward if able
-observe new state
-update q matrix
-
-q-matrix entry = current s/a value + (reward + discounted(random(successor s/a value) - current s/a value)) 
-
-state string: 5 cells (current, North, south, east, west) 1 = can, 2 = wall, 0 = empty eg 10012
-actions = n(move north), s(move south), e(move east), w(move west), p(pick up can)
-state-action key = 10002 + action, eg 
-
-objects:
-q-matrix: dict for q(sa):value
-episode: steps, total reward
-simulation: grid, robot, 
-grid: x by x cells
-cell: has a state (can/no can with 1/2 chance of either)
-robot: has a sensor state (eg 10102), current coordinate, q-matrix: chooses own actions
-
-state/action entry: just a key/value pair in dict. key = state-action (eg 10021P), value = reqard
-
-flow:
-
-start in random cell
-observe state
-choose actions with epsilon greedy
-perform action
-get reward if able
-observe new state
-update old q-state with new value
-
-for each episode:
-    run x number of steps
-"""
 import random
 from re import I
-from time import sleep
 from numpy import random
+import numpy as np
 from typing import List
 
 
@@ -70,7 +28,6 @@ class Grid:
         for column in grid:
             for _ in range(0, self.size):
                 cell = Cell()
-                # print(cell)
                 column.append(cell)
 
         return grid
@@ -78,6 +35,7 @@ class Grid:
     def calculate_state(self, xy: List):
         x = xy[0]
         y = xy[1]
+        state = f"{self.grid[x][y]}"
 
         surrounding_cells = [
             # North, south, east, west
@@ -87,13 +45,12 @@ class Grid:
             [0, -1],
         ]
 
-        state = f"{self.grid[x][y]}"
         for coordinate in surrounding_cells:
             new_x = x + coordinate[0]
             new_y = y + coordinate[1]
 
             if 0 <= new_x < SIZE and 0 <= new_y < SIZE:
-                state += f"{self.grid[x+coordinate[0]][y+coordinate[1]].has_can}"
+                state += f"{self.grid[x+coordinate[0]][y+coordinate[1]]}"
             else:
                 state += f"2"
 
@@ -105,56 +62,51 @@ class Robot:
         self.current_xy = xy
         self.current_state = grid.calculate_state(self.current_xy)
         self.q_matrix = {}
-        # pick up can, move north, move south, move eats, move west
+        # pick up can, move north, move south, move east, move west
         self.actions = ["p", "n", "s", "e", "w"]
         self.discount_rate = DISCOUNT_RATE
 
     def progress_state(self, grid: Grid, epsilon):
-        state_actions = {}
-        state_action = None
         """
-        calculaue all possible sucessor states
+        calculaue all possible successor states
         check if any exist in q matrix
         if not, add to matrix with value = 0
-        if so, add state to list of possibler actions
+        if so, add state to list of possible actions
         """
+        state_actions = {}
+        state_action = None
         for action in self.actions:
             state_action = self.current_state + action
             state_value = self.q_matrix.setdefault(state_action, 0)
             state_actions[state_action] = state_value
-            # print(state, state_value)
 
         selected_state_action = self.select_state_action(state_actions, epsilon)
-        # successor_state_actions = self.calculate_successor_state_actions(selected_state_action, grid)
         state_value = self.calculate_state_value(selected_state_action, grid)
         self.q_matrix[selected_state_action] += state_value
         self.current_xy = self.determine_next_position(selected_state_action)
         self.current_state = grid.calculate_state(self.current_xy)
 
-        # print(selected_state_action, state_value)
         return state_value
 
     def select_state_action(self, state_actions, epsilon):
-        """find best reward or random select
+        """
+        find best reward or random select
         1-e chance of selecting best option (ties broken at random)
         else random select
         """
         selected_state_action = None
         probability_of_best_choice = 1 - epsilon
         random_choice = round(random.uniform(0, 1), 2)
-        # print(f"random choice: {random_choice} {probability_of_best_choice}")
 
         if random_choice > probability_of_best_choice:
             selected_state_action = random.choice(list(state_actions), 1)
         else:
             max_value = state_actions[max(state_actions, key=state_actions.get)]
-            # print(f"max val: {max_value}")
             best_actions = [
                 state_action
                 for state_action in state_actions
                 if state_actions.get(state_action) == max_value
             ]
-            # print(best_actions)
             selected_state_action = random.choice(best_actions, 1)
 
         return selected_state_action[0]
@@ -181,7 +133,6 @@ class Robot:
             state = next_state + action
             state_value = self.q_matrix.setdefault(state, 0)
             state_actions[state] = state_value
-            # print(state, state_value)
 
         return state_actions
 
@@ -190,8 +141,9 @@ class Robot:
         current_value = self.q_matrix[state_action]
         state_reward = self.calculate_state_reward(state_action, grid)
         max_successor_value = self.calculate_max_successor_value(state_action, grid)
-        return state_reward + N_VALUE * (
-            self.discount_rate * max_successor_value - current_value
+
+        return N_VALUE * (
+            state_reward + (self.discount_rate * max_successor_value) - current_value
         )
 
     def calculate_state_reward(self, state_action, grid: Grid):
@@ -286,7 +238,9 @@ class Simulation:
             count -= 1
             if count % 100 == 0:
                 print(episode.total_reward)
-        print(f"training avg: {sum(ep.total_reward for ep in self.training_episodes)/EPISODE_COUNT}")
+        print(
+            f"training avg: {sum(ep.total_reward for ep in self.training_episodes)/EPISODE_COUNT}"
+        )
 
     def run_test_simulation(self):
         print("test results")
@@ -301,10 +255,13 @@ class Simulation:
             episode.run_episode(self.robot)
             self.test_episodes.append(episode)
             count -= 1
-        sum = 0
+        totals = []
+        sum_total = 0
         for ep in self.test_episodes:
-            sum += ep.total_reward
-        print(sum / EPISODE_COUNT)
+            sum_total += ep.total_reward
+            totals.append(ep.total_reward)
+        print(sum(totals) / len(totals))
+        print(np.std(totals))
 
 
 class Episode:
@@ -326,14 +283,6 @@ class Episode:
             count -= 1
         self.average_reward = self.total_reward / self.step_count
 
-
-# grid = Grid(SIZE)
-# rob = Robot([0,0], grid)
-# print(grid.calculate_state(rob.current_xy))
-# print(rob.progress_state(grid))
-
-# episode = Episode()
-# episode.run_episode(Robot([0, 0], episode.grid))
 
 simulation = Simulation()
 simulation.run_full_simulation()
